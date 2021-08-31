df73b95a718dc96e67a4a92f7c9cb5dc559b57bf
Huyen-ntt <nguyenhuyenpy98@gmail.com>
2021-08-31 11:31:40 +0700

    Ex3

---
+++ src/test/java/ex3/App.java
@@ -0,0 +1,13 @@
+package ex3;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Input month: ");
+        int month= sc.nextInt();
+        System.out.println("Input year: ");
+        int year= sc.nextInt();
+        Number n1= new Number();
+        System.out.println( "Month: "+ month+ " have to: "+ n1.monthofYear(month,year));
+    }
+}
---
+++ src/test/java/ex3/Number.java
@@ -0,0 +1,32 @@
+package ex3;
+
+public class Number {
+    public String monthOfYear(int month, int year) {
+        String result = "";
+        for (int i = 1; i < month; i++) {
+            switch (month) {
+                case 1:
+                case 3:
+                case 5:
+                case 7:
+                case 8:
+                case 10:
+                case 12:
+                    result = 31 + " day ";
+                    break;
+                case 4:
+                case 6:
+                case 9:
+                case 11:
+                    result = 30 + " day  ";
+                    break;
+                case 2:
+                    result += month % 4 == 0 ? 29 : 28+" day";
+                    break;
+                default:
+                    break;
+            }
+        }
+        return  result;
+    }
+}
